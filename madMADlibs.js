696ada9cd0c0e4073e56b34deb9c27ff5508ad08
MatthewsPLaceholder <theworkingplaceholder@gmail.com>
2024-08-14 15:47:00 +0000

    madMAdlibs.js

---
+++ madMADlibs.js
@@ -0,0 +1,13 @@
+let verb1 = "running"  
+let noun1 = "cat"   
+let place1 = "museum"   
+
+
+prompt("enter a verb")
+
+let output ="the" + verb1 + noun1 + "was going to the" + place1   
+
+console.log(output)  
+console.log(6+9)        
+
+
